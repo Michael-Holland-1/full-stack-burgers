ca7458285c7dfa304ecc78d410ba7651c8e3a8da
Rhonda Glennon <rhondaglennon@gmail.com>
2018-03-04 13:06:53 -0800

    update controllers and templates, put heading into main

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -22,9 +22,8 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   console.log("inside the post")
-  burger.insertOne(["burger_name", "devoured"
-]), [
-    req.body.burger_name, req.body.devoured
+  burger.insertOne(["burger_name"]), [
+    req.body.burger_name
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
@@ -38,7 +37,7 @@ router.put("/api/burgers/:id", function(req, res) {
   console.log("condition", condition);
 
   burger.updateOne({
-    devoured: req.body.devoured
+    devoured: true
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
